b86e8514d08067a12b62a01b04f9c923fd865cc0
“javier” <javiermontes97@gmail.com>
2026-02-05 00:32:44 +0100

    Add gunicorn to requirements.txt

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 # Instalar dependencias Python
 pip install -r requirements.txt
 
-# Compilar Tailwind (asegúrate de que npm/yarn esté disponible en el entorno)
-npx tailwindcss -i ./static/src/input.css -o ./static/dist/styles.css --minify
-
 # Migraciones
 python manage.py migrate --noinput
 
