499695e39f4f9c638df8c4fa0fdb2f835e35337b
“javier” <javiermontes97@gmail.com>
2026-02-05 00:11:18 +0100

    Install and Add whitenoise in settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,3 +142,5 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = 'home' 
 LOGIN_URL = 'login'  # Si el usuario no está logueado, será redirigido aquí
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
